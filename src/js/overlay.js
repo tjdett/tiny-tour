da5c679efc61518ac8ab0baf295e21402a73a32a
Lee Newson <lee.newson@tiny.cloud>
2019-04-02 11:57:04 +1000

    Reverted back to using ES6 directly, instead of TypeScript

--- src/js/overlay.js
+++ src/js/overlay.js
@@ -1,25 +1,12 @@
-declare const tinymce: any;
+const init = (config, zIndex) => {
+  const baseZIndex = zIndex || 101;
+  const blockerZIndex = baseZIndex + 2;
 
-interface ElementConfig {
-  helpUrl?: string;
-  helpHtml?: string;
-  buttons?: string[];
-  onAction?: (api: any) => void;
-  onClose?: () => void;
-  onSubmit?: (api: any) => void;
-}
-
-interface Config {
-  bind?: Record<string, ElementConfig>;
-  help?: Record<string, ElementConfig>;
-}
-
-const init = (config: Config) => {
   const bindActionOverlays = () => {
     // Bind the relevant help elements
     for (const selector of Object.keys(config.bind)) {
       const elmConfig = config.bind[selector];
-      const elements = document.querySelectorAll<HTMLElement>(selector);
+      const elements = document.querySelectorAll(selector);
       elements.forEach((elm) => {
         bindOverlayElement(elm, elmConfig);
       });
@@ -27,7 +14,7 @@ const init = (config: Config) => {
   };
 
   const showHelpOverlays = () => {
-    const overlays: { elm: HTMLElement, config: ElementConfig }[] = [];
+    const overlays = [];
 
     // Build the root element
     const wrapper = buildRoot();
@@ -63,7 +50,7 @@ const init = (config: Config) => {
     // Bind the relevant help elements
     for (const selector of Object.keys(config.help)) {
       const elmConfig = config.help[selector];
-      const elements = document.querySelectorAll<HTMLElement>(selector);
+      const elements = document.querySelectorAll(selector);
       elements.forEach((elm) => {
         overlayElement(elm);
         overlays.push({ elm, config: elmConfig });
@@ -71,7 +58,7 @@ const init = (config: Config) => {
     }
   };
 
-  const buildRoot = (zIndex: number = 101): HTMLElement => {
+  const buildRoot = () => {
     const root = document.createElement('div');
     root.style.backgroundColor = 'rgba(0, 0, 0, 0.6)';
     root.style.position = 'fixed';
@@ -79,18 +66,18 @@ const init = (config: Config) => {
     root.style.right = '0';
     root.style.top = '0';
     root.style.bottom = '0';
-    root.style.zIndex = '' + zIndex;
+    root.style.zIndex = baseZIndex;
     return root;
   };
 
-  const buildBlocker = (zIndex: number = 103): HTMLElement => {
+  const buildBlocker = () => {
     const blocker = document.createElement('div');
     blocker.style.position = 'fixed';
     blocker.style.left = '0';
     blocker.style.right = '0';
     blocker.style.top = '0';
     blocker.style.bottom = '0';
-    blocker.style.zIndex = '' + zIndex;
+    blocker.style.zIndex = blockerZIndex;
 
     return blocker;
   };
@@ -110,26 +97,26 @@ const init = (config: Config) => {
     return close;
   };
 
-  const overlayElement = (element: HTMLElement) => {
+  const overlayElement = (element) => {
     if (element.getAttribute('style')) {
       element.dataset.tinyPrevStyles = element.getAttribute('style');
     }
     element.style.backgroundColor = '#fff';
-    element.style.zIndex = '102';
+    element.style.zIndex = baseZIndex + 1;
     element.style.border = '1px solid blue'
   };
 
-  const restoreElement = (element: HTMLElement) => {
+  const restoreElement = (element) => {
     element.setAttribute('style', element.dataset.tinyPrevStyles);
   };
 
-  const bindOverlayElement = (element: HTMLElement, elmConfig: ElementConfig) => {
+  const bindOverlayElement = (element, elmConfig) => {
     element.addEventListener('click', () => {
       openDialog(elmConfig);
     });
   };
 
-  const openDialog = (elmConfig: ElementConfig) => {
+  const openDialog = (elmConfig) => {
     const buttons = [];
     if (!elmConfig.buttons) {
       buttons.push({
